7bb89cbae094eca3dbb4f1b22df9d38f388145c1
hcan <hcan@trinitysoft.co.kr>
2020-07-13 15:07:04 +0900

    메인뷰 수정.

--- Sast.Viewer/ViewModels/MainViewModel.cs
+++ Sast.Viewer/ViewModels/MainViewModel.cs
@@ -1,12 +1,53 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace Sast.Viewer.ViewModels
 {
 	public class MainViewModel : BindableBase
 	{
+		#region Fields
+
+		private string _command;
+		private string _message;
+
+		#endregion
+
 		#region Constructors
 
 		public MainViewModel()
+		{
+			CalculatorCommand = new DelegateCommand(Calculator);
+		}
+
+		#endregion
+
+		#region Properties
+
+		public ICommand CalculatorCommand
+		{
+			get;
+			set;
+		}
+
+		public string Command
+		{
+			get { return _command; }
+			set { SetProperty(ref _command, value); }
+		}
+
+		public string Message
+		{
+			get { return _message; }
+			set { SetProperty(ref _message, value); }
+		}
+
+		#endregion
+
+		#region Private methdos
+
+		private void Calculator()
 		{
 
 		}
